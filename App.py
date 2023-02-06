6e3c5b5e990d63eb0983c93a3601e68014b0fb94
username016 <mohammed@sherkhan.ca>
2023-02-05 23:31:13 -0500

    Building Block #1

--- App.py
+++ App.py
@@ -1,2 +1,14 @@
 # Implementing the interactivity with the user
 
+import re
+
+print("Welcome to a text-based adventure game called Beta, please enter your name")
+name = input("")
+x = re.findall("[A-Za-z]", name)
+if x:
+  print("Hi",name)       
+else:
+  print("Enter letters only !!")
+
+# User enters their's name
+
